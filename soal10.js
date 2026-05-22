03f0eae28c83981f0811dcab5414cb1b5bab33b8
ejiputra123-cmyk <ejiputra123@gmail.com>
2026-05-22 08:36:06 +0700

    reworking soal 10 js

--- soal10.js
+++ soal10.js
@@ -1,11 +1,54 @@
 const prompt = require('prompt-sync') ({sigint : true});
 
-const judulBuku = prompt('Masukkan judul buku : ');
-const penrbit = String(prompt('Masukkan penerbit : '));
-const jumlahBuku = parseInt(prompt('Masukkan jumlah buku : '));
-const tanggalBeli = prompt('Masukkan tanggal beli : ');
+let judulBuku;
+let penerbit;
+let jumlahBuku;
+let tanggalBeli;
 
+while (true) {
+    judulBuku = prompt('Masukkan judul buku: ')
+
+    if (judulBuku.trim() === "") {
+        console.log('Error: harus terisi!');
+    } else {
+        break;
+    }
+
+}
+
+while(true) {
+    penerbit = String(prompt('Masukkan penerbit : '));
+
+    if (penerbit.trim() === "") {
+        console.log('Error: harus terisi!');
+    } else {
+        break;
+    }
+    
+}
+
+while(true) {
+    jumlahBuku = (prompt('Masukkan jumlah buku : '));
+
+    if (jumlahBuku.trim() === "") {
+        console.log('Error: harus terisi!');
+    } else {
+        break;
+    }
+}
+
+while(true) {
+    tanggalBeli = prompt('Masukkan tanggal beli : ');
+
+    if (tanggalBeli.trim() === "") {
+        console.log('Error: harus terisi!');
+    } else {
+        break;
+    }
+}
+
+console.log(`\n--- DATA BUKU ---`);
 console.log(`Judul buku : ${judulBuku}`);
-console.log(`Penerbit : ${penrbit}`);
+console.log(`Penerbit : ${penerbit}`);
 console.log(`Jumlah buku : ${jumlahBuku}`);
 console.log(`Tanggal Pembelian : ${tanggalBeli}`);
